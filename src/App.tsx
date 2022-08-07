687540f3464b13777c34a757057b71b36bb3a0d2
hassankotti <hkotti95@gmail.com>
2022-08-07 15:12:31 +0300

    /home removed from base

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import Home from './pages/Home';
 import ReportStudies from './pages/ReportStudies';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
-
+import { createBrowserHistory } from 'history';
 function App() {
-
+  const history = createBrowserHistory();
   return (
-    <BrowserRouter basename="/">
-      <Route>
-        <Route exact path='/' render={() => <Home />} />
-        <Route exact path='/home' render={() => <Home />} />
-        <Route path='/reports_and_studies' render={() => <ReportStudies />} />
-        <Route path='/articles' render={() => <Articles />} />
-        <Route path='/article/:id' render={() => <Article />} />
-      </Route>
-    </BrowserRouter>
+    <Home />
+    // <Router history={history}>
+    //   <Route>
+    //     <Route exact path='/' render={() => <Home />} />
+    //     <Route exact path='/home' render={() => <Home />} />
+    //     <Route path='/reports_and_studies' render={() => <ReportStudies />} />
+    //     <Route path='/articles' render={() => <Articles />} />
+    //     <Route path='/article/:id' render={() => <Article />} />
+    //   </Route>
+    // </Router>
 
   );
 }
