c4eec6c6113ed311351480baea1afbff035a099f
hassankotti <hkotti95@gmail.com>
2022-08-07 15:35:07 +0300

    hp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Router } from "react-router-dom";
+import { Route, Router, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
 import ReportStudies from './pages/ReportStudies';
 import Articles from './pages/Articles';
@@ -8,16 +8,15 @@ import { createBrowserHistory } from 'history';
 function App() {
   const history = createBrowserHistory();
   return (
-    <Home />
-    // <Router history={history}>
-    //   <Route>
-    //     <Route exact path='/' render={() => <Home />} />
-    //     <Route exact path='/home' render={() => <Home />} />
-    //     <Route path='/reports_and_studies' render={() => <ReportStudies />} />
-    //     <Route path='/articles' render={() => <Articles />} />
-    //     <Route path='/article/:id' render={() => <Article />} />
-    //   </Route>
-    // </Router>
+    <BrowserRouter basename='/'>
+      <Route>
+        <Route exact path='/' render={() => <Home />} />
+        <Route exact path='/home' render={() => <Home />} />
+        <Route path='/reports_and_studies' render={() => <ReportStudies />} />
+        <Route path='/articles' render={() => <Articles />} />
+        <Route path='/article/:id' render={() => <Article />} />
+      </Route>
+    </BrowserRouter>
 
   );
 }
